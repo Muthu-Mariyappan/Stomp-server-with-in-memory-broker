cab4a90a60a77946bd5d82657a0f0dcd8e2e03c5
Muthu G <MuthuMariyappan.G@Honeywell.com>
2019-12-06 11:45:20 +0530

    Added size config

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -12,14 +12,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").withSockJS();
+        
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
-
-
+        //registry.setCacheLimit(1024*1024);
         //   Use this for enabling a Full featured broker like RabbitMQ
 
         /*
@@ -29,5 +29,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setClientLogin("guest")
                 .setClientPasscode("guest");
         */
+    }
+    
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(50 * 1024 * 1024);
+        registration.setSendTimeLimit(20 * 10000);
+        registration.setSendBufferSizeLimit(3* 512 * 1024);
     }
 }
